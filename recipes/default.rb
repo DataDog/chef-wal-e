9c78ca61bed3ce19b450188bce7c3dbb2cbe829d
Seth Rosenblum <seth@datadoghq.com>
2015-11-19 17:33:38 -0500

    Add AWS region attribute and environment variable.

--- recipes/default.rb
+++ recipes/default.rb
@@ -50,6 +50,7 @@ end
 
 vars = { 'AWS_ACCESS_KEY_ID'     => node[:wal_e][:aws_access_key],
          'AWS_SECRET_ACCESS_KEY' => node[:wal_e][:aws_secret_key],
+         'AWS_REGION'            => node[:wal_e][:aws_region],
          'WALE_S3_PREFIX'        => node[:wal_e][:s3_prefix] }
 
 vars.each do |key, value|
