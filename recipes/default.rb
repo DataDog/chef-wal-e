e1ffc14eb544af3202cd251a00ae74e465f21548
Cory Stephenson <aevin@me.com>
2017-02-21 13:30:41 -0600

    Fix virtualenv owner.

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,7 +14,7 @@ unless node[:wal_e][:virtualenv].nil?
   include_recipe "python::virtualenv"
 
   python_virtualenv node[:wal_e][:virtualenv][:path] do
-    user node[:wal_e][:pip_user]
+    owner node[:wal_e][:pip_user]
     group node[:wal_e][:group]
     interpreter node[:wal_e][:virtualenv][:interpreter] if node[:wal_e][:virtualenv][:interpreter]
     action :create
